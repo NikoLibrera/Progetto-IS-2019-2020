78616e4b160812eb5a2846a80c72696df5c6a315
carmi <carmi@DESKTOP-P8FAUJ9>
2020-01-21 16:29:28 +0100

    Creata la classe Progetto, modifiche al file header.css e header.jsp

---
+++ SIMPLIFY3D/src/model/Progetto.java
@@ -0,0 +1,106 @@
+package model;
+
+import java.sql.Blob;
+
+public class Progetto {
+	
+	
+	private static  int PROGRESS_ID=0;
+	
+	
+	private int id_progetto;
+	private String titolo;
+	private String descrizione;
+	private Blob file_modello;
+	private Blob immagine;
+	private String consigli;
+	private String categoria;
+	private int versione;
+	private String username;
+	
+	public Progetto()
+	{
+		
+	}
+	
+	
+	public Progetto(String titolo, String descrizione, Blob file_modello, Blob immagine, String consigli,
+			String categoria, String username) {
+		super();
+		this.titolo = titolo;
+		this.descrizione = descrizione;
+		this.file_modello = file_modello;
+		this.immagine = immagine;
+		this.consigli = consigli;
+		this.categoria = categoria;
+		this.username = username;
+		this.versione=1;
+		Progetto.PROGRESS_ID++;
+	}
+
+
+
+
+
+
+	public int getId_progetto() {
+		return id_progetto;
+	}
+	public void setId_progetto(int id_progetto) {
+		this.id_progetto = id_progetto;
+	}
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public Blob getFile_modello() {
+		return file_modello;
+	}
+	public void setFile_modello(Blob file_modello) {
+		this.file_modello = file_modello;
+	}
+	public Blob getImmagine() {
+		return immagine;
+	}
+	public void setImmagine(Blob immagine) {
+		this.immagine = immagine;
+	}
+	public String getConsigli() {
+		return consigli;
+	}
+	public void setConsigli(String consigli) {
+		this.consigli = consigli;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public int getVersione() {
+		return versione;
+	}
+	public void setVersione(int versione) {
+		this.versione = versione;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void incVersion()
+	{
+		this.versione++;
+	}
+
+}
--- WebContent/Header.jsp
+++ WebContent/Header.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html"
-    pageEncoding="UTF-8" import="java.util.*,model.Utente" %>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1" import="java.util.*,model.Utente" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +12,7 @@
 	<div class="intestazione">
 	
 		
-		<img src="images/logo.PNG">
+		 <a class="logo" href="HomePage.jsp"><img  class="imgLogo" src="images/logo.PNG" ></a>
 			<form action="#" class="formRicerca">
 				
 				<input class="ricerca" type="search" placeholder="Digita qui per ricercare un progetto...">
@@ -22,13 +22,13 @@
 		Utente u=(Utente)session.getAttribute("utente");
 		if(u==null){%>
 				
-			  <form action="LoginPage.jsp" class="formAccedi">
+			  <form action="#" class="formAccedi">
 				<button class="button_accedi">Accedi o Registrati</button>
 			</form>
 	<% 	}else{ %>
 	<form action="#" class="formAccedi">
-				<a href="ProfiloPersonale.jsp"><img src="images/utente_profilo.png"></a>
-				<a href="Logout"><img src="images/logout.png" ></a>
+				<a class="profilo" href="ProfiloPersonale.jsp"><button class="imgProfilo"><img src="images/utente_profilo.png" ><h3><%= u.getNome() + "\n" + u.getCognome() %></h3></button></a>
+				<a class="logout" href="Logout"><button class="imgLogout"><img src="images/logout.png"><h3>Logout</h3></button></a>
 			</form>
         <%} %>
         
